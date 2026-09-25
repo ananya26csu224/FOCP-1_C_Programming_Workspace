cea380c402384b98f1113a32873f5ea0c45dcd4a
ananya26csu224 <ananya26csu224@ncuindia.edu>
2026-09-25 12:48:01 +0530

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int studentID = 101;
+    int age = 20;
+    float percentage = 85.5;
+    char grade = 'A';
+
+    printf("Student ID: %d\n", studentID);
+    printf("Age: %d\n", age);
+    printf("Percentage: %.2f\n", percentage);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
